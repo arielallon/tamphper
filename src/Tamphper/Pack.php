ff2e454c95c6cc22fa0dfe4ac7324e73e3f2c4cd
Ariel Allon <arielallon@gmail.com>
2014-06-01 09:31:32 -0500

    Bunch of additions. Not much ready yet. More of a check-in.

--- src/Tamphper/Pack.php
+++ src/Tamphper/Pack.php
@@ -2,7 +2,59 @@
 
 namespace Tamphper;
 
-class Pack
+abstract class Pack
 {
-    
+    protected $_attrName;
+    protected $_possibilities;
+    protected $_maxChoices;
+
+    protected $_bitWindowWidth = null;
+    protected $_itemWidnowWidth = null;
+    protected $_bitset = null;
+
+    protected $_maxGuid = null;
+
+    protected $_meta = array();
+
+    protected $_encoding;
+    protected $_encodedBitset;
+
+    public function __construct($attrName, $possibilities, $maxChoices)
+    {
+        $this->_attrName = $attrName;
+        $this->_possibilities = $possibilities;
+        $this->_maxChoices = $maxChoices;
+    }
+
+    public static function build($attrName, $possibilities, $maxChoices)
+    {
+        $numPossibilities = length($possibilities);
+        if ($maxChoices * log($numPossibilities, 2) < $numPossibilities) {
+
+        }
+        else {
+
+        }
+    }
+
+    public function toArray()
+    {
+        $output = array(
+            'encoding'            => $this->_encoding,
+            'attr_name'           => $this->_attrName,
+            'possibilities'       => $this->_possibilities,
+            'pack'                => $this->_encodedBitset,
+            'item_window_width'   => $this->_itemWindowWidth,
+            'bit_window_width'    => $this->_bitWindowWidth,
+            'max_choices'         => $this->_maxChoices,
+        );
+        return array_merge($output, $this->_meta);
+    }
+
+    abstract public function encode($guid);
+
+    public function finalizePack()
+    {
+
+    }
 }
--- src/Tamphper/PackSet.php
+++ src/Tamphper/PackSet.php
@@ -4,5 +4,53 @@ namespace Tamphper;
 
 class PackSet
 {
-  
+    public function __construct()
+    {
+
+    }
+
+    public function addAttribute()
+    {
+
+    }
+
+    public function addBufferedAttribute()
+    {
+
+    }
+
+    public function getAttributes()
+    {
+
+    }
+
+    public function getPackFor($attr)
+    {
+
+    }
+
+    public function pack()
+    {
+
+    }
+
+    public function buildPack()
+    {
+
+    }
+
+    public function buildUnorderedPack()
+    {
+
+    }
+
+    public function toArray()
+    {
+
+    }
+
+    public function toJson()
+    {
+
+    }
 }
